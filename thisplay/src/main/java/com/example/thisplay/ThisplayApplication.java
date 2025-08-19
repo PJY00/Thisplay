ba77c530894cfa0179f2eec5aad900d81720c9c7
HJ1263532 <osisland2918@gmail.com>
2025-08-19 15:47:20 +0900

    First file commit

---
+++ thisplay/src/main/java/com/example/thisplay/ThisplayApplication.java
@@ -0,0 +1,13 @@
+package com.example.thisplay;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ThisplayApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ThisplayApplication.class, args);
+	}
+
+}
---
+++ thisplay/src/test/java/com/example/thisplay/ThisplayApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.thisplay;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ThisplayApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
