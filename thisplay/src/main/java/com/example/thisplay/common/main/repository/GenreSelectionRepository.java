547c492a2ccc9a7bde2ac2758b26218dd3a9d060
HJ1263532 <osisland2918@gmail.com>
2025-09-21 22:49:41 +0900

    refactor: 주석 작성

--- thisplay/src/main/java/com/example/thisplay/common/main/repository/GenreSelectionRepository.java
+++ thisplay/src/main/java/com/example/thisplay/common/main/repository/GenreSelectionRepository.java
@@ -3,5 +3,6 @@ package com.example.thisplay.common.main.repository;
 import com.example.thisplay.common.main.entity.GenreSelection;
 import org.springframework.data.jpa.repository.JpaRepository;
 
+//필요 없으면 Entity 부분과 함께 지우기
 public interface GenreSelectionRepository extends JpaRepository<GenreSelection,Long> {
 }
