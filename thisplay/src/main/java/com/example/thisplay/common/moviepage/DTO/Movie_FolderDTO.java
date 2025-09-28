9d3237221993f3bddc71029f8dbb962c0004ee8e
HJ1263532 <osisland2918@gmail.com>
2025-09-29 01:53:53 +0900

    feat: 제공 정보 제한을 위한 DTO설정

---
+++ thisplay/src/main/java/com/example/thisplay/common/moviepage/DTO/Movie_FolderDTO.java
@@ -0,0 +1,15 @@
+package com.example.thisplay.common.moviepage.DTO;
+
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+@Getter
+@Setter
+@NoArgsConstructor
+@AllArgsConstructor
+public class Movie_FolderDTO {
+    private Long id;
+    private String name;
+}
