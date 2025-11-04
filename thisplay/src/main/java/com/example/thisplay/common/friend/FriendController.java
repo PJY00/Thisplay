785cbe0e8797dd8401274350b6161db210e03be3
Lee Eun Gyo <ialice119@naver.com>
2025-11-04 17:06:56 +0900

    feat : friend DTO 생성

--- thisplay/src/main/java/com/example/thisplay/common/friend/FriendController.java
+++
@@ -1,4 +0,0 @@
-package com.example.thisplay.common.friend;
-
-public class FriendController {
-}
---
+++ thisplay/src/main/java/com/example/thisplay/common/friend/controller/FriendController.java
@@ -0,0 +1,4 @@
+package com.example.thisplay.common.friend.controller;
+
+public class FriendController {
+}
