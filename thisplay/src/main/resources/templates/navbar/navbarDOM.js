f4b43f291983e7625033a871869fca2aa43fcb53
JiyeongPark <163852701+PJY00@users.noreply.github.com>
2025-11-19 14:47:57 +0900

    fix: login navbar 임시 수정 완 추가 수정필요 사항: 유저 프로필, 유저 아이디 받아오기, 로그아웃시 경로 문제 해결 필요

--- thisplay/src/main/resources/templates/navbar/navbarDOM.js
+++ thisplay/src/main/resources/templates/navbar/navbarDOM.js
@@ -1,10 +1,117 @@
+// import { logout } from "../../static/js/utils/auth.js";
+// function createNavbar() {
+//   // css 삽입
+//   const link = document.createElement("link");
+//   link.rel = "stylesheet";
+//   // link.href = "../navbar/navbar.css"; // 경로는 필요에 따라 수정
+//   link.href = "/thisplay/src/main/resources/templates/navbar/navbar.css"; // 경로는 필요에 따라 수정
+//   document.head.appendChild(link);
+
+//   // HTML 삽입
+//   const navbarHTML = `
+//     <nav class="navbar">
+//       <div class="nav-left">
+//         <div class="logo">THISPLAY</div>
+//       </div>
+
+//       <div class="nav-center">
+//         <div class="search-box">
+//           <input
+//             type="text"
+//             id="search-input"
+//             placeholder="검색어를 입력하세요..."
+//           />
+//           <button id="search-btn" aria-label="검색">
+//             <i class="uil uil-search"></i>
+//           </button>
+//         </div>
+//       </div>
+
+//       <div class="nav-right">
+//         <ul class="menu">
+//           <li id="login-btn"><button type="button">LOG IN</button></li>
+//           <li id="profile-menu" class="hidden">
+//             <img src="/thisplay/src/main/resources/templates/navbar/profile.png" alt="프로필" id="profile-img" />
+//             <ul class="dropdown hidden">
+//               <li><a href="#">마이페이지</a></li>
+//               <li><a href="#">설정</a></li>
+//               <li><a href="#" id="logout-btn">로그아웃</a></li>
+//             </ul>
+//           </li>
+//         </ul>
+//       </div>
+//     </nav>
+//   `;
+
+//   /// body에 navbar 추가
+//   document.body.insertAdjacentHTML("afterbegin", navbarHTML);
+//   const profileMenu = document.getElementById("profile-menu");
+//   const dropdown = profileMenu.querySelector(".dropdown");
+//   const profileImg = document.getElementById("profile-img");
+//   const logoutBtn = document.getElementById("logout-btn");
+
+//   // login 버튼 연결
+//   const loginBtn = document.querySelector("#login-btn button");
+//   if (loginBtn) {
+//     loginBtn.addEventListener("click", () => {
+//       window.location.href = "../login_join/login.html";
+//     });
+//   }
+
+//   const isLoggedIn = () => !!localStorage.getItem("token");
+//   console.log(loginBtn);
+
+//   if (isLoggedIn()) {
+//     loginBtn.classList.add("hidden");
+//     profileMenu.classList.remove("hidden");
+//   } else {
+//     loginBtn.classList.remove("hidden");
+//     profileMenu.classList.add("hidden");
+//   }
+
+//   document.addEventListener("DOMContentLoaded", () => {
+//     createNavbar();
+
+//     const loginBtn = document.querySelector("#login-btn");
+//     const profileMenu = document.getElementById("profile-menu");
+
+//     const isLoggedIn = () => !!localStorage.getItem("token");
+
+//     if (isLoggedIn()) {
+//       loginBtn?.classList.add("hidden");
+//       profileMenu?.classList.remove("hidden");
+//     } else {
+//       loginBtn?.classList.remove("hidden");
+//       profileMenu?.classList.add("hidden");
+//     }
+//   });
+
+//   // dropdown
+
+//   if (profileMenu && dropdown) {
+//     profileMenu.addEventListener("mouseenter", () => {
+//       dropdown.classList.remove("hidden");
+//     });
+
+//     profileMenu.addEventListener("mouseleave", () => {
+//       dropdown.classList.add("hidden");
+//     });
+//   }
+
+//   document.getElementById("logout-btn").addEventListener("click", () => {
+//     logout();
+//   });
+// }
+
+// document.addEventListener("DOMContentLoaded", createNavbar);
+
 import { logout } from "../../static/js/utils/auth.js";
+
 function createNavbar() {
-  // css 삽입
+  // CSS 삽입
   const link = document.createElement("link");
   link.rel = "stylesheet";
-  link.href = "../navbar/navbar.css"; // 경로는 필요에 따라 수정
-  link.href = "/thisplay/src/main/resources/templates/navbar/navbar.css"; // 경로는 필요에 따라 수정
+  link.href = "../navbar/navbar.css";
   document.head.appendChild(link);
 
   // HTML 삽입
@@ -16,22 +123,18 @@ function createNavbar() {
 
       <div class="nav-center">
         <div class="search-box">
-          <input
-            type="text"
-            id="search-input"
-            placeholder="검색어를 입력하세요..."
-          />
-          <button id="search-btn" aria-label="검색">
-            <i class="uil uil-search"></i>
-          </button>
+          <input id="search-input" type="text" placeholder="검색어를 입력하세요..." />
+          <button id="search-btn"><i class="uil uil-search"></i></button>
         </div>
       </div>
 
       <div class="nav-right">
         <ul class="menu">
           <li id="login-btn"><button type="button">LOG IN</button></li>
+
           <li id="profile-menu" class="hidden">
-            <img src="/thisplay/src/main/resources/templates/navbar/profile.png" alt="프로필" id="profile-img" />
+            <img src="/thisplay/src/main/resources/templates/navbar/profile.png" 
+                 alt="프로필" id="profile-img" />
             <ul class="dropdown hidden">
               <li><a href="#">마이페이지</a></li>
               <li><a href="#">설정</a></li>
@@ -43,64 +146,45 @@ function createNavbar() {
     </nav>
   `;
 
-  /// body에 navbar 추가
   document.body.insertAdjacentHTML("afterbegin", navbarHTML);
+
+  // 요소 셀렉터
+  const loginLi = document.getElementById("login-btn");
+  const loginBtn = loginLi.querySelector("button");
   const profileMenu = document.getElementById("profile-menu");
   const dropdown = profileMenu.querySelector(".dropdown");
-  const profileImg = document.getElementById("profile-img");
-  const logoutBtn = document.getElementById("logout-btn");
 
-  // login 버튼 연결
-  const loginBtn = document.querySelector("#login-btn button");
-  if (loginBtn) {
-    loginBtn.addEventListener("click", () => {
-      window.location.href = "../login_join/login.html";
-    });
-  }
-
+  // 로그인 여부
   const isLoggedIn = () => !!localStorage.getItem("token");
-  console.log(loginBtn);
 
+  // 로그인/로그아웃 상태 반영
   if (isLoggedIn()) {
-    loginBtn.classList.add("hidden");
+    loginLi.classList.add("hidden");
     profileMenu.classList.remove("hidden");
   } else {
-    loginBtn.classList.remove("hidden");
+    loginLi.classList.remove("hidden");
     profileMenu.classList.add("hidden");
   }
 
-  document.addEventListener("DOMContentLoaded", () => {
-    createNavbar();
-
-    const loginBtn = document.querySelector("#login-btn");
-    const profileMenu = document.getElementById("profile-menu");
-
-    const isLoggedIn = () => !!localStorage.getItem("token");
-
-    if (isLoggedIn()) {
-      loginBtn?.classList.add("hidden");
-      profileMenu?.classList.remove("hidden");
-    } else {
-      loginBtn?.classList.remove("hidden");
-      profileMenu?.classList.add("hidden");
-    }
+  // 로그인 버튼 클릭
+  loginBtn.addEventListener("click", () => {
+    window.location.href = "../login_join/login.html";
   });
 
   // dropdown
+  profileMenu.addEventListener("mouseenter", () => {
+    dropdown.classList.remove("hidden");
+  });
 
-  if (profileMenu && dropdown) {
-    profileMenu.addEventListener("mouseenter", () => {
-      dropdown.classList.remove("hidden");
-    });
+  profileMenu.addEventListener("mouseleave", () => {
+    dropdown.classList.add("hidden");
+  });
 
-    profileMenu.addEventListener("mouseleave", () => {
-      dropdown.classList.add("hidden");
-    });
-  }
-
+  // 로그아웃
   document.getElementById("logout-btn").addEventListener("click", () => {
     logout();
   });
 }
 
+// create navbar
 document.addEventListener("DOMContentLoaded", createNavbar);
